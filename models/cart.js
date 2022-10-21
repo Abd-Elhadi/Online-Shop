7a39875f24b1414156354e2ad1e6adc23227195d
Abdelhadi Omar <abdelhadioumar@gmail.com>
2022-10-21 13:40:26 +0200

    Cart model

--- models/cart.js
+++ models/cart.js
@@ -24,7 +24,7 @@ module.exports = class Cart {
       // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
-        updatedProduct.qty = updatedProduct.qty + 1;
+        updatedProduct.qty += 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
       } else {
@@ -35,6 +35,33 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
+    });
+  }
+
+  static deleteProduct(id, price) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) return;
+    });
+    console.log(fileContent);
+    const updatedCart = { ...JSON.parse(fileContent) };
+    const product = updatedCart.products.find(prod => prod.id === id);
+    productQty = product.qty;
+    updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+
+    updatedCart.totalPrice -= (productQty * price);
+    fs.writeFile(p, JSON.stringify(updatedCart), err => {
+      console.log(err);
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
