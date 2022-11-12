bb695c3dbdd521d8177aeff8e7a2e02886ed35bd
Abdelhadi Omar <abdelhadioumar@gmail.com>
2022-11-13 01:43:52 +0200

    Fix Cart view

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,16 +56,12 @@ exports.getCart = (req, res, next) => {
   console.log(req.user.cart);
   req.user
   .getCart()
-    .then(cart => {
-      return cart.getProducts().then(products => {
+    .then(products => {
         res.render('shop/cart', {
           path: '/cart',
           pageTitle: 'Your Cart',
           products: products
         });
-      }).catch(err => {
-        console.log(err);
-      });
     })
     .catch(err => {
       console.log(err);
@@ -79,42 +75,13 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product);
   })
   .then(result => {
-    // console.log('You?')
+    console.log('You?')
     console.log(result);
+    res.redirect('/cart');
   })
   .catch(err => {
     console.log(err);
   });
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
