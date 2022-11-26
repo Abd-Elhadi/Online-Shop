9acde4f2a78ff86cfa63945bb2429d2230750c99
Abdelhadi Omar <abdelhadioumar@gmail.com>
2022-11-26 22:53:51 +0200

    AWS SES

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,45 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
+
+const aws = require('aws-sdk');
+
+const ses = new aws.SES({region: "us-east-1"});
+
+function sesTest(emailTo, emailFrom, subject, message) {
+    let params = {
+        Destination: {
+          /* required */
+          ToAddresses: [emailTo]
+        },
+        Message: {
+          /* required */
+          Body: {
+            /* required */
+            Html: {
+              Charset: "UTF-8",
+              Data: message
+            }
+            // ,
+            // Text: {
+            //   Charset: "UTF-8",
+            //   Data: `Hi  $\{name\}!Your Login OTP is $\{otp\}`
+            // }
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: subject
+          }
+        },
+        Source: emailFrom,
+        /* required */
+        ReplyToAddresses: [emailFrom]
+    };
+    return ses.sendEmail(params).promise();
+}
+
 
 exports.getLogin = (req, res, next) => {
     // console.log(req.flash('error'));
@@ -72,7 +111,6 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const condfirmPassword = req.body.condfirmPassword;
-
     User.findOne({email: email})
     .then(userDoc => {
         if (userDoc) {
@@ -90,6 +128,23 @@ exports.postSignup = (req, res, next) => {
                 return user.save();    
             })
             .then(result => {
+                const emailTo = email;
+                const emailFrom = "abdelhadiomar.coder@hotmail.com";
+                const message = `<p>You successfully signed up!</p>
+                                <br>
+                                <p>Regards,<br/>
+                                Abdelhadi</p>`;
+                const subject = 'Successful signup!';
+                
+                sesTest(emailTo, emailFrom, subject, message)
+                .then(val => {
+                    console.log('got this back', val)
+                    console.log('Successful');
+                })
+                .catch(err => {
+                    console.log(err);
+                    console.log('Something went wrong');
+                });
                 res.redirect('login');
             })
     })
@@ -104,3 +159,81 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message
+    });
+};
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        console.log(token);
+        User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                req.flash('error', 'No account with that email found.');
+                return res.redirect('/reset');
+            }
+            user.resetToken = token;
+            user.resetTokenExpiration = Date.now() + 3600000;
+            return user.save();
+        })
+        .then(result => {
+            const emailFrom = 'abdelhadiomar.coder@hotmail.com';
+            const emailTo = req.body.email;
+            const subject = 'Password reset';
+            const message = `
+                            <p>You requested a password reset.</p>
+                            <p>Click this <a href ="http://localhost:3000/reset${token}">link</a> to set a new password.</p>
+                            <p>Regards,<br/>
+                            Abdelhadi</p>`;
+            sesTest(emailTo, emailFrom, subject, message)
+            .then(val => {
+                console.log('got this back', val)
+                console.log('Successful');
+                res.redirect('/');
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    })
+};
+
+exports.getNewPassword = (req, res, next) => {
+    console.log('hello');
+    const token = req.params.token;
+    console.log(token);
+    User.findOne({resetToken : token, resetTokenExpiration: {$gt: Date.now()}})
+    .then(user => {
+        let message = req.flash('error');
+        if (message.length > 0) {
+            message = message[0];
+        } else {
+            message = null;
+        }
+        res.render('auth/new-password', {
+            path: '/new-password',
+            pageTitle: 'New Password',
+            errorMessage: message,
+            userId: user._id.toString()
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
