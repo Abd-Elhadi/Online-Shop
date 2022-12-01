3fd00df8570e593c6edc0c71049116a18fda4faf
Abdelhadi Omar <abdelhadioumar@gmail.com>
2022-12-01 23:20:33 +0200

    Sending http request from client-side

--- controllers/admin.js
+++ controllers/admin.js
@@ -235,8 +235,8 @@ exports.getProducts = (req, res, next) => {
 //   })
 //   // Product.deleteById(prodId);
 // }
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
   .then(product => {
     if (!product) {
@@ -247,10 +247,14 @@ exports.postDeleteProduct = (req, res, next) => {
   })
   .then(() => {
     console.log('DESTROYED PRODUCT');
-    res.redirect('/admin/products');
+    res.status(200).json({
+      message: "Success!"
+    })
   })
   .catch(err => {
-    return get500Error(err, next);
+    res.status(500).json({
+      message: "Deleting product failed"
+    });
   }); 
 }
 
---
+++ public/js/admin.js
@@ -0,0 +1,18 @@
+const deleteProduct = (button) => {
+    const prodId = button.parentNode.querySelector('[name=productId]').value;
+    const csrf = button.parentNode.querySelector('[name=_csrf]').value;
+    
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
