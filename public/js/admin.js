449ef978f0e6dd445f711b0993871fa391aa34e7
Abdelhadi Omar <abdelhadioumar@gmail.com>
2022-12-01 23:29:41 +0200

    Update the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,7 @@ const deleteProduct = (button) => {
     const prodId = button.parentNode.querySelector('[name=productId]').value;
     const csrf = button.parentNode.querySelector('[name=_csrf]').value;
     
+    const productElement = button.closest('article');
 
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
@@ -10,7 +11,12 @@ const deleteProduct = (button) => {
         }
     })
     .then(result => {
-        console.log(result);
+        return result.json()
+    })
+    .then(data => {
+        console.log(data);
+        // productElement.parentNode.removeChild(productElement);
+        productElement.remove(); // Work with modren browsers
     })
     .catch(err => {
         console.log(err);
